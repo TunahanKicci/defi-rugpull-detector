d5ee7d36ddfc10261a4d998f8ee87b5a84042878
Tunahan Kicci <tunahankicci@gmail.com>
2025-12-05 18:11:12 +0300

    liste güncellendi

--- frontend/src/data/ethereumTokens.js
+++ frontend/src/data/ethereumTokens.js
@@ -7,7 +7,84 @@ const ethereumTokens = [
   {
     symbol: 'BNB',
     address: '0xB8c77482e45F1F44dE1745F52C74426C631bDD52',
+  },
+  {
+    symbol: 'USDC',
+    address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+  },
+  {
+    symbol: 'stETH',
+    address: '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84',
+  },
+  {
+    symbol: 'wstETH',
+    address: '0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0',
+  },
+  {
+    symbol: 'WBTC',
+    address: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
+  },
+  {
+    symbol: 'wBETH',
+    address: '0xa2E3356610840701BDf5611a53974510Ae27E2e1',
+  },
+  {
+    symbol: 'WETH',
+    address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+  },
+  {
+    symbol: 'LINK',
+    address: '0x514910771AF9Ca656af840dff83E8264EcF986CA',
+  },
+  {
+    symbol: 'USDS',
+    address: '0xdC035D45d973E3EC169d2276DDab16f1e407384F',
+  },
+  {
+    symbol: 'LEO',
+    address: '0x2AF5D2aD76741191D15Dfe7bF6aC92d4Bd912Ca3',
+  },
+  {
+    symbol: 'weETH',
+    address: '0xCd5fE23C85820F7B72D0926FC9b05b43E359b7ee',
+  },
+  {
+    symbol: 'USDe',
+    address: '0x4c9EDD5852cd905f086C759E8383e09bff1E68B3',
+  },
+  {
+    symbol: 'cbBTC',
+    address: '0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf',
+  },
+  {
+    symbol: 'SHIB',
+    address: '0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE',
+  },
+  {
+    symbol: 'NICK',
+    address: '0x9c427fe6fac60c512f990dbef4371d6bb3e78d1e',
+  },
+  {
+    symbol: 'SUPERCELL',
+    address: '0xbbad61c50057889c3877bb2392f2fa8e4d3bb1df',
+  },
+  {
+    symbol: 'HASBRO',
+    address: '0xe089a30e5bec50587d2c7dce0bbd04a5462d9d6f',
+  },
+  {
+    symbol: 'ZAIMELLA',
+    address: '0xe3f27913e8bd6971ffe8530c2f5173faaa0aaa1d',
+  },
+  {
+    symbol: 'COACH',
+    address: '0xeb61ba960d3970f29f7b8db8795498a33494cbff',
+  },
+  {
+    symbol: 'OTHERSIDE',
+    address: '0xcd9bdd2bf0f3f28a9767f948be38bd7b9af44596',
   }
+  
 ]
 
 export default ethereumTokens
